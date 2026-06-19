e639a0242d7bca6e1f29c3d306d94c2b7d9af763
Fariha <fariha1032004@gmail.com>
2026-06-20 01:10:53 +0600

    Added VeterinaryAppointment.php Model

---
+++ laravel-app/app/Models/VeterinaryAppointment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VeterinaryAppointment extends Model
+{
+    protected $primaryKey = 'appointment_id';
+
+    protected $fillable = [
+        'pet_id',
+        'vet_id',
+        'requested_by',
+        'appointment_date',
+        'reason',
+        'status',
+        'notes',
+    ];
+
+    protected $casts = [
+        'appointment_date' => 'date',
+    ];
+
+    public function pet()
+    {
+        return $this->belongsTo(Pet::class, 'pet_id', 'pet_id');
+    }
+
+    public function vet()
+    {
+        return $this->belongsTo(User::class, 'vet_id', 'user_id');
+    }
+
+    public function requester()
+    {
+        return $this->belongsTo(User::class, 'requested_by', 'user_id');
+    }
+}
